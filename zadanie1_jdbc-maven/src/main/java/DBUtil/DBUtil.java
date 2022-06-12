b8a42244e09460967bb30b19dab2bf8b5e4a0516
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-06-12 15:27:55 +0200

    add DBUtil.java

---
+++ zadanie1_jdbc-maven/src/main/java/DBUtil/DBUtil.java
@@ -0,0 +1,28 @@
+
+package DBUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBUtil {
+    private static final String DB_DRIVER_CLASS="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static final String DB_USERNAME="adam";
+	private static final String DB_PASSWORD="adam";
+	private static final String DB_URL ="jdbc:sqlserver://LAPTOP-442E11CB\\SQLEXPRESS:1433";
+	
+	private static Connection connection = null;
+	static{
+		try {
+			Class.forName(DB_DRIVER_CLASS);
+			connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection(){
+		return connection;
+	}
+}
