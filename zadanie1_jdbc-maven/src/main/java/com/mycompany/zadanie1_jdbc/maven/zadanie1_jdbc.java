100787188a68d9e92c2d8b4a7624f185826a0969
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-06-12 15:21:21 +0200

    add dependencies

---
+++ zadanie1_jdbc-maven/src/main/java/com/mycompany/zadanie1_jdbc/maven/zadanie1_jdbc.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.zadanie1_jdbc.maven;
+
+
+public class zadanie1_jdbc {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
